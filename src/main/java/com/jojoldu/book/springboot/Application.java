e12fa77edc6abaa7223c880bc5840e9fb190d033
theGchoi <choiejwl187@gmail.com>
2022-10-24 19:31:53 +0900

    - JPA(자바 표준 ORM)를 이용해 데이터베이스 관리 - 요구사항 분석으로 만드려는 서비스 이해도 상승(게시판 CRUD 및 API 로그인과 권한 관리) - application.properties로 h2 데이터베이스 사용 및 mysql로 출력 언어 변경 - JPA Auditing으로 생성시간/수정시간 자동화

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,11 +2,14 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing                                                      // JPA Auditing 활성화
 @SpringBootApplication                                                  // 스프링 부트의 자동설정, 스프링 Bean 읽기와 생성 모두 자동으로 설정됨.
 public class Application {                                              // 해당 어노테이션(@SpringBootApplication)이 있는 위치부터 설정을 읽어가기 때문에 항상 프로젝트 최상단에 위치해야 함.
     public static void main(String[] args){                             // 앞으로 만들 프로젝트의 메인 클래스
         SpringApplication.run(Application.class, args);                 // SpringApplication.run = 내장 WAS(Web Application Server, 웹 애플리케이션 서버, 외부에 WAS를 두지 않고 애플리케이션을 실행할 때 내부에서 WAS를 실행하는 것) 실행-> 톰캣 설치 필요 X
 
     }                                                                   //
+
 }
---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest                                                                                                         // 별다른 설정 없이 @SpringBootTest 사용 시 자동으로 H2 데이터베이스를 실행해줌
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After                                                                                                              // @After = Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정.
+    public void cleanup(){                                                                                              // 보통은 배포 전 전체 테스트를 수행할 때 테스트 간 데이터 침범을 막기 위해 사용
+        postsRepository.deleteAll();                                                                                    // 여러 테스트가 동시에 수행되면 테스트용 데이터베이스인 H2에 데이터가 그대로 담아있어 다음 테스트가 실패할 수 있음
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("jojoldu@gmail.com").build());        // postsRepository.save = posts 테이블에 insert/update 쿼리를 실행
+                                                                                                                        // id 값이 있다면 update, 없다면 insert 쿼리가 실행됨
+        //when
+        List<Posts> postsList = postsRepository.findAll();                                                              // postsRepository.findAll = posts 테이블에 있는 모든 데이터를 조회해오는 메소드
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+       //given
+       LocalDateTime now = LocalDateTime.of(1994,8,16,07,30,0);
+       postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+       //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>> createDate= "+posts.getCreatedDate()+", modifiedDate= "+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
