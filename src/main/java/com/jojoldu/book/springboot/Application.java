65a4f8e418f4c5679dbbafe4f29500059f2255cc
theGchoi <choiejwl187@gmail.com>
2022-11-02 20:49:38 +0900

    - 스프링 시큐리티 적용 	- test 패키지에도 application.properties 생성 	- Apllication 클래스에 JPA Auditing 활성화 어노테이션(@EnableJpaAuditing) JpaConfig클래스 생성하여 분할 	- 각각의 TestController들에 임의 사용자 인증 추가 	- SpringBootTest에 MockMvc 사용을 위해 //when //then 수정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing                                                      // JPA Auditing 활성화
+// @EnableJpaAuditing                                                      // JPA Auditing 활성화
 @SpringBootApplication                                                  // 스프링 부트의 자동설정, 스프링 Bean 읽기와 생성 모두 자동으로 설정됨.
 public class Application {                                              // 해당 어노테이션(@SpringBootApplication)이 있는 위치부터 설정을 읽어가기 때문에 항상 프로젝트 최상단에 위치해야 함.
     public static void main(String[] args){                             // 앞으로 만들 프로젝트의 메인 클래스
